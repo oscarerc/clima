e8743fda3e7d08844142b486c3416e322dda3586
unknown <oscareduardorodriguezcastro3@gmail.com>
2024-01-28 22:14:20 -0500

    iniciando 3

--- app.js
+++ app.js
@@ -5,7 +5,26 @@ import chalk from "chalk";
 const API_KEY= "d37b8e3be4b5922670f73e0bf80d21f1";
 
 async function getWeather(city) {
-    
+    try {
+        let endpoint =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+
+        const responer = await axios.get(endpoint,{
+            params: {
+                q:city,
+                appid: API_KEY,
+                units:"metric"
+            }
+        });
+        console.log(response);
+        return Response.data;
+
+    } catch (err) {
+
+        console.log(chalk.red.bold(err));
+        throw new error(
+            `No es posible conectarla informacion de la ciudad: ${city}`
+        );
+    }
 }
 
 
@@ -17,6 +36,8 @@ function getData() {
         console.log(chalk.red("Por favor proporciona una ciudad validad"));
         console.log(chalk.red.bold("Ejecuta de la siguiente forma : node app.j [nombre de la ciudad]"));
     }
+    // promesa
+    getWeather(city).then().catch();
 }
 
 
